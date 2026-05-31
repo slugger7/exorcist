c49d0081cbeb78c0b12d35feefb5627283b95044
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2026-05-31 07:36:01 +0200

    revert(server): unique tags constraint

--- apps/server/migrations/20260528062709_unique-tags-people.down.sql
+++
@@ -1,3 +0,0 @@
-alter table drop constraint unique_media_tag;
-
-alter table drop constraint unique_media_person;
--- apps/server/migrations/20260528062709_unique-tags-people.up.sql
+++
@@ -1,4 +0,0 @@
--- TODO: remove the newest records that will prevent this constraint from being added
-alter table media_tag add constraint unique_media_tag unique (media_id, tag_id);
-
-alter table media_person add constraint unique_media_person unique (media_id, person_id);
